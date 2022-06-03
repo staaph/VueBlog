374b06a9656b082e37ac39497ca672c83562024a
staaph <contact.staaph@gmail.com>
2022-06-04 00:58:54 +0200

    styling

--- src/components/FeaturedSection.vue
+++ src/components/FeaturedSection.vue
@@ -1,17 +1,22 @@
 <template>
-  <main class="grid grid-cols-3 gap-5">
+  <main class="grid grid-cols-3 gap-10">
     <article v-for="(article, key) in articles" :key="key">
-      <div class="flex flex-row gap-x-3">
-        <h1 v-text="'0' + (key + 1)" class="font-bold text-3xl text-gray-400" />
-        <div class="flex flex-col text-sm">
-          <span v-text="article.category" class="font-thin text-xs" />
-          <span v-text="article.description" class="font-bold" />
-          <div class="flex flex-row gap-x-2">
-            <span v-text="article.author + ','" />
-            <span v-text="article.date" />
+      <a :href="`/${key}`">
+        <div class="flex flex-row gap-x-3">
+          <h1
+            v-text="'0' + (key + 1)"
+            class="font-bold text-3xl text-gray-400"
+          />
+          <div class="flex flex-col text-sm">
+            <span v-text="article.category" class="font-thin text-xs" />
+            <span v-text="article.description" class="font-bold" />
+            <div class="flex flex-row gap-x-2">
+              <span v-text="article.author + ','" />
+              <span v-text="article.date" />
+            </div>
           </div>
         </div>
-      </div>
+      </a>
     </article>
   </main>
 </template>
